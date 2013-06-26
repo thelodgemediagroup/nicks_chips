1ef1b324004350da29ee62c66b689b949bc27f7b
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-06-26 10:19:22 -0400

    Style index page content, setup compass

--- dev/wp-content/themes/nickschips/index.php
+++ dev/wp-content/themes/nickschips/index.php
@@ -10,30 +10,60 @@
 
 	<div class="wrapper">
 
-		<header>
-			<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/header.gif" />
-		</header>
+		<section class="main-content">
 
-		<nav>
-			<ul>
-				<li><a href="">About Nicks</a></li>
-				<li><a href="">Products</a></li>
-				<li><a href="">Order Online</a></li>
-				<li><a href="">News &amp; Info</a></li>
-				<li><a href="">Specials</a></li>
-				<li><a href="">Contact Nicks</a></li>
-			</ul>
-		</nav>
+			<header>
+				<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/header.gif" alt="Nicks Chips - Delivering the hottest brand in town to your store!" />
+			</header>
 
-		<div class="container">
-			<div class="content">
-			</div><!--/ .content -->
-			<aside class="right-sidebar">
-			</aside><!--/ .right-sidebar -->
-		</div><!--/ .container -->
+			<nav>
+				<ul>
+					<li><a href="">About Nicks</a></li>
+					<li><a href="">Products</a></li>
+					<li><a href="">Order Online</a></li>
+					<li><a href="">News &amp; Info</a></li>
+					<li><a href="">Specials</a></li>
+					<li><a href="">Contact Nicks</a></li>
+				</ul>
+			</nav>
+
+			<div class="container">
+				<div class="page-content">
+
+					<h2>Thank you for visiting Nicks Snack Foods website!</h2>
+
+					<h1>Enter our WIN $150 Instagram Contest</h1>
+
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/contest-img.jpg" alt="Enter our WIN $150 Instagram Contest" />
+
+					<p>Just follow us at Instagram@NicksChips and submit a photo of yourself with any bag of Nicks Snacks using the hashtag #luvNicksChips</p>
+
+					<p>One winner will be picked at the end of each month from June-December 2013! ENTER NOW!</p>
+
+				</div><!--/ .page-content -->
+
+				<aside class="right-sidebar">
+
+					<h2>Nicks HOT News Flash!</h2>
+
+					<p><a href="">ENTER TO WIN $150. Instagram Contest</a></p>
+
+					<p><a href="">NEW Hot Habenero &amp; Nacho Cheeze Tortilla Chips</a></p>
+
+					<p><a href="">New Nicks Flaming Hot Rod heating up the neighborhood</a></p>
+
+					<p><a href="">Download our 2013 Nicks Product Line Sell Sheet here</a></p>
+
+					<p><a href="">Visit us at the AFPD Trade Show at the Suburban Showplace September 2013</a></p>
+
+				</aside><!--/ .right-sidebar -->
+			</div><!--/ .container -->
+
+		</section><!--/ .main-content -->	
 
 		<aside class="main-sidebar">
 			<div class="chips-graphic">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/nicks-logo.jpg" alt="Nicks - The Hottest Brand in Town!" />
 			</div><!--/ .chips-graphic -->
 			<div class="instagram">
 			</div><!--/ .instagram -->
