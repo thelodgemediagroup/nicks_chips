4a0e36c0dbede4d6c77c6f8dc13b8e50c6fb04f7
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-06-26 13:33:22 -0400

    Create wordpress loop in index page for templates. Create index and about

--- dev/wp-content/themes/nickschips/index.php
+++ dev/wp-content/themes/nickschips/index.php
@@ -3,15 +3,11 @@
 			<div class="container">
 				<div class="page-content">
 
-					<h2>Thank you for visiting Nicks Snack Foods website!</h2>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-					<h1>Enter our WIN $150 Instagram Contest</h1>
+						<?php the_content(); ?>
 
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/contest-img.jpg" alt="Enter our WIN $150 Instagram Contest" />
-
-					<p>Just follow us at Instagram@NicksChips and submit a photo of yourself with any bag of Nicks Snacks using the hashtag #luvNicksChips</p>
-
-					<p>One winner will be picked at the end of each month from June-December 2013! ENTER NOW!</p>
+					<?php endwhile; endif; ?>
 
 					<footer>
 
