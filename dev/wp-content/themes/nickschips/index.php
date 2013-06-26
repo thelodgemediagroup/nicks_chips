6752c09b223b4cbaff32f995ba84d957aa5f87aa
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-06-26 12:39:00 -0400

    Social media icons, footer

--- dev/wp-content/themes/nickschips/index.php
+++ dev/wp-content/themes/nickschips/index.php
@@ -13,6 +13,18 @@
 
 					<p>One winner will be picked at the end of each month from June-December 2013! ENTER NOW!</p>
 
+					<footer>
+
+						<div class="social-media">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/twitter.jpg" />
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/facebook.jpg" />
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/instagram.jpg" />
+						</div><!--/ .social-media -->
+						
+						<p class="copyright">&copy; <?php echo date('Y'); ?>, Nicks Chips LLC. All rights reserved.</p>
+
+					</footer>					
+
 				</div><!--/ .page-content -->
 
 				<?php get_sidebar('right'); ?>
--- dev/wp-content/themes/nickschips/sidebar-main.php
+++ dev/wp-content/themes/nickschips/sidebar-main.php
@@ -3,5 +3,6 @@
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/nicks-logo.jpg" alt="Nicks - The Hottest Brand in Town!" />
 			</div><!--/ .chips-graphic -->
 			<div class="instagram">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/instagram-temp.jpg" alt="Nicks Chips Instagram" />
 			</div><!--/ .instagram -->
 		</aside><!--/ .main-sidebar -->
