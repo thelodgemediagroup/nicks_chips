5fdb02c65deac9103cab918df33acf7eae0449f6
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-10-14 09:17:24 -0400

    Finish updates and backup database, no contact page

--- dev/wp-content/themes/nickschips/sidebar-right.php
+++ dev/wp-content/themes/nickschips/sidebar-right.php
@@ -2,17 +2,17 @@
 
 					<h2 class="coop">Nicks HOT News Flash!</h2>
 
-					<p><a href="">WIN EMINEM PRIZE PACK<br />If token is inside your bag</a></p>
+					<p><a href="/contest/">WIN EMINEM PRIZE PACK<br />If token is inside your bag</a></p>
 
 					<p><a href="/contest/">ENTER TO WIN $150<br />Instagram Contest</a></p>
 
-					<p><a href="/contest/">NEW! Hot&amp;Spicy, BBQ and Original Pork Rinds</a></p>
+					<p><a href="/pork-rinds/">NEW! Hot&amp;Spicy, BBQ and Original Pork Rinds</a></p>
 
-					<p><a href="/new-products/">NEW! Family Size Tortilla Chips</a></p>
+					<p><a href="/tortilla-chips/">NEW! Family Size Tortilla Chips</a></p>
 
-					<p><a href="/new-products/">NEW! Family Size Potato Chips</a></p>
+					<p><a href="/potato-chips/">NEW! Family Size Potato Chips</a></p>
 
-					<p><a href="/new-products/">NEW Hot Habenero &amp; Nacho Cheeze Tortilla Chips</a></p>
+					<p><a href="/tortilla-chips/">NEW Hot Habenero &amp; Nacho Cheeze Tortilla Chips</a></p>
 
 					<p><a href="/product-sell-sheet/">Download our 2013 Nicks Product Line Sell Sheet here</a></p>
 
